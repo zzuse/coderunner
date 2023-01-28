fc769a5b067210b2f0ffd6e578b1e5378942e34f
zzuse <zzuse@outlook.com>
2023-01-27 20:21:28 -0600

    math learning trie tree

---
+++ Maths/trieTree.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+
+class TrieNode:
+
+	def __init__(self, letter):
+		self.letter = letter  # 每个节点中存储一个字母
+		self.childnode = [None] * 26
+		self.isend = False    # isend表示单词是否结束
+
+
+class TrieTree:
+	"""
+		must be lowercase letters
+	"""
+	def __init__(self):
+		self.root = TrieNode('/')
+
+	def insert(self, word):
+		p = self.root
+		for w in word:
+			childnode = p
+			index = ord(w) - ord('a')
+			if p.childnode[index] == None:
+				p.childnode[index] = TrieNode(w)
+			p = p.childnode[index]
+		p.isend = True
+		
+	def find(self, word):
+		p = self.root
+		for w in word:
+			index = ord(w) - ord('a')
+			if p.childnode[index] == None:
+				return False
+			p = p.childnode[index]
+		if not p.isend:
+			return False
+		else:
+			return True
+	
+	def startswith(self, prefix):
+		p = self.root
+		for w in prefix:
+			index = ord(w) - ord('a')
+			if p.childnode[index] == None:
+				return False
+			p = p.childnode[index]
+		return True
+	
+	
+if __name__ == "__main__":
+	trie = TrieTree()
+	trie.insert("apple")
+	print("insert apple \n")
+	print("find apple {}\n".format(trie.find("apple")))
+	print("find app {}\n".format(trie.find("app")))
+	print("startswith app {}\n".format(trie.startswith("app")))
+	trie.insert("app")
+	print("insert app \n")
+	print("find app {}\n".format(trie.find("app")))
+
+		
