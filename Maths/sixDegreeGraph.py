534f70a44d89af897ab7e5dddb0b528278ad212d
zzuse <zzuse@outlook.com>
2023-02-02 19:57:34 -0600

    bidirection bfs search

--- Maths/sixDegreeGraph.py
+++ Maths/sixDegreeGraph.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import random
+import queue
+
 
 class Node:
     
@@ -19,9 +21,9 @@ class UserNodes:
         for i in range(10):
             friend_a_id = random.randint(0,num-1)
             friend_b_id = random.randint(0,num-1)
-            print(friend_a_id,friend_b_id)
             if friend_a_id == friend_b_id:
                 continue
+            print("relation ship: {} -- {}".format(friend_a_id, friend_b_id))
             self.user_nodes[friend_a_id].friends.add(friend_b_id)
             self.user_nodes[friend_b_id].friends.add(friend_a_id)
 
@@ -49,9 +51,59 @@ class UserNodes:
                 queue.append(friend_id)
                 visited.add(friend_id)
                 self.user_nodes[friend_id].degree = self.user_nodes[cur_user_id].degree + 1
-                print("{} degree friends: {}".format(self.user_nodes[friend_id].degree, friend_id))
+                print("Node {} has {} degree friends: {}".format(user_id, self.user_nodes[friend_id].degree, friend_id))
+        if len(queue) <=0:
+            print("Node {} no more friends".format(user_id))
+            
+    def get_next_degree_friend(self, que_in, visited_in):
+        que_ret = queue.Queue()
+        visited_ret = set()
+        while not que_in.empty():
+            cur_user_id = que_in.get()
+            for friend_id in self.user_nodes[cur_user_id].friends:
+                if visited_in.__contains__(friend_id):
+                    continue
+                que_ret.put(friend_id)
+                visited_ret.add(friend_id)
+        return que_ret, visited_ret
+    
+    def has_overlap(self, visited_a, visited_b):
+        return len(visited_a & visited_b) > 0
+        
+    def bi_bfs_search(self, friend_a_id, friend_b_id):
+        """
+        bidirectional bfs search, to find shortest path between two user
+        """
+        if friend_a_id > len(self.user_nodes) or friend_b_id > len(self.user_nodes):
+            return
+        if friend_a_id == friend_b_id:
+            return 0
+        
+        que_a = queue.Queue()
+        que_b = queue.Queue()
+        que_a.put(friend_a_id)
+        que_b.put(friend_b_id)
+        visited_a = set()
+        visited_b = set()
+        visited_a.add(friend_a_id)
+        visited_b.add(friend_b_id)
+        
+        degree_a, degree_b, max_degree = 0, 0, 20
+        while degree_a + degree_b < max_degree:
+            degree_a += 1
+            que_a, visited_a = self.get_next_degree_friend(que_a, visited_a)
+            if self.has_overlap(visited_a, visited_b):
+                print("user {} and {} have {} degree relation".format(friend_a_id, friend_b_id, degree_a + degree_b))
+                return degree_a + degree_b
+            degree_b += 1
+            que_b, visited_b = self.get_next_degree_friend(que_b, visited_b)
+            if self.has_overlap(visited_a, visited_b):
+                print("user {} and {} have {} degree relation".format(friend_a_id, friend_b_id, degree_a + degree_b))
+                return degree_a + degree_b
+        return -1
 
 
 if __name__ == "__main__":
     users = UserNodes(6)
     users.bfs_search(0)
+    users.bi_bfs_search(0,5)
