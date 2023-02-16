c37d1af6f2a13132000453c53f59c81d827477e0
zzuse <zzuse@outlook.com>
2023-02-15 23:01:22 -0600

    liner iris

--- Maths/liner/iris.py
+++ Maths/liner/iris.py
@@ -12,3 +12,25 @@ iris = pd.DataFrame(iris['data'], columns = ['Sepal_Length', 'Sepal_Width', 'Pet
 
 iris['Species'] = species
 print(iris)
+
+
+def plot_iris(iris, col1, col2):
+	import seaborn as sns
+	import matplotlib.pyplot as plt
+	
+	sns.lmplot(x = col1, y = col2,
+				data = iris,
+				hue = "Species",
+				fit_reg = False)
+	
+	plt.xlabel(col1)
+	
+	plt.ylabel(col2)
+	
+	plt.title('Iris species shown by color')
+	
+	plt.show()
+	
+plot_iris(iris, 'Petal_Width', 'Sepal_Length')
+
+plot_iris(iris, 'Sepal_Width', 'Sepal_Length')
