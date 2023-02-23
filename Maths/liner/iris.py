67ba3a5fe337189b8511c547c671e93e3574b759
zzuse <zzuse@outlook.com>
2023-02-22 23:52:45 -0600

    iris

--- Maths/liner/iris.py
+++ Maths/liner/iris.py
@@ -8,6 +8,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
 
 iris = datasets.load_iris()
 
@@ -70,6 +71,49 @@ print(iris_test_features.shape)
 
 print(iris_test_labels.shape)
 
-plot_iris(iris, 'Petal_Width', 'Petal_Length')
+# plot_iris(iris, 'Petal_Width', 'Sepal_Length')
 
-plot_iris(iris, 'Sepal_Width', 'Sepal_Length')
+# plot_iris(iris, 'Sepal_Width', 'Sepal_Length')
+
+KNN_mod = KNeighborsClassifier(n_neighbors = 3)
+
+KNN_mod.fit(np.asarray(iris_train_features), np.asarray(iris_train_labels))
+
+iris_test = pd.DataFrame(iris_test_features, columns = num_cols)
+
+iris_test['predicted'] = KNN_mod.predict(np.asarray(iris_test_features))
+
+iris_test['correct'] = [1 if x == z else 0 for x, z in zip(iris_test['predicted'], iris_test_labels)]
+
+accuracy = 100.0 * float(sum(iris_test['correct'])) / float(iris_test.shape[0])
+
+print(accuracy)
+
+levels = {0:'setosa', 1:'versicolor', 2:'virginica'}
+
+iris_test['Species'] = [levels[x] for x in iris_test['predicted']]
+
+markers = {1:'^', 0:'o'}
+
+colors = {'setosa':'blue', 'versicolor':'green',}
+
+def plot_shapes(df, col1,col2,  markers, colors):
+	
+	ax = plt.figure(figsize=(6, 6)).gca() # define plot axis
+	
+	for m in markers: # iterate over marker dictioary keys
+		for c in colors: # iterate over color dictionary keys
+			df_temp = df[(df['correct'] == m)  & (df['Species'] == c)]
+			sns.regplot(x = col1, y = col2,
+						data = df_temp, 
+						fit_reg = False,
+						scatter_kws={'color': colors[c]},
+						marker = markers[m],
+						ax = ax)
+	plt.xlabel(col1)
+	plt.ylabel(col2)
+	plt.title('Iris species by color')
+	plt.show()
+
+plot_shapes(iris_test, 'Petal_Width', 'Sepal_Length', markers, colors)
+plot_shapes(iris_test, 'Sepal_Width', 'Sepal_Length', markers, colors)
