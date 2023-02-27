2aac84294030c980fcc3efbde9fb854db3eac980
zzuse <zzuse@outlook.com>
2023-02-26 22:46:09 -0600

    for alg

---
+++ typescript/src/day1/TwoCrystalBalls.ts
@@ -0,0 +1,17 @@
+export default function two_crystal_balls(breaks: boolean[]): number {
+    const jmpAmount = Math.floor(Math.sqrt(breaks.length));
+    let i = jmpAmount;
+    for (; i < breaks.length; i += jmpAmount) {
+        if ( breaks[i] ) {
+            break;
+        }
+    }
+
+    i -= jmpAmount;
+    for (let j = 0; j < jmpAmount && i < breaks.length; ++i){
+        if (breaks[i]){
+            return i;
+        }
+    }
+    return -1;
+}
