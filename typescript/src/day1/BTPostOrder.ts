36f77a24d7515eebc7c7f1deec0543b7426d7e3b
zzuse <zzuse@outlook.com>
2023-03-05 23:29:33 -0600

    Trees

--- typescript/src/day1/BTPostOrder.ts
+++ typescript/src/day1/BTPostOrder.ts
@@ -1,3 +1,16 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if(!curr) {
+        return path;
+    }
+    //pre
+    //recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+    //post
+    path.push(curr.value);
+    return path;
+}
 
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
