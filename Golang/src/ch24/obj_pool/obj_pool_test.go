3730e2bba335bf6201889383a6eb9eb0458a3556
zzuse <zzuse@outlook.com>
2023-03-13 23:08:03 -0500

    csp pool

---
+++ Golang/src/ch24/obj_pool/obj_pool_test.go
@@ -0,0 +1,56 @@
+package obj_pool
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+	"time"
+)
+
+type ReusableObj struct {
+}
+
+type ObjPool struct {
+	bufChan chan *ReusableObj
+}
+
+func NewObjPool(numOfObj int) *ObjPool {
+	objPool := ObjPool{}
+	objPool.bufChan = make(chan *ReusableObj, numOfObj)
+	for i := 0; i < numOfObj; i++ {
+		objPool.bufChan <- &ReusableObj{}
+	}
+	return &objPool
+}
+
+func (p *ObjPool) GetObj(timeout time.Duration) (*ReusableObj, error) {
+	select {
+	case ret := <-p.bufChan:
+		return ret, nil
+	case <-time.After(timeout):
+		return nil, errors.New("time out")
+	}
+}
+
+func (p *ObjPool) ReleaseObj(obj *ReusableObj) error {
+	select {
+	case p.bufChan <- obj:
+		return nil
+	default:
+		return errors.New("overflow")
+	}
+}
+
+func TestObjPool(t *testing.T) {
+	pool := NewObjPool(10)
+	for i := 0; i < 11; i++ {
+		if v, err := pool.GetObj(time.Second * 1); err != nil {
+			t.Error(err)
+		} else {
+			fmt.Printf("%T\n", v)
+			if err := pool.ReleaseObj(v); err != nil {
+				t.Error(err)
+			}
+		}
+	}
+}
---
+++ Golang/src/ch25/obj_cache/sync_pool_test.go
@@ -0,0 +1,49 @@
+package obj_cache
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+)
+
+// because GC. not very useful
+func TestSyncPool(t *testing.T) {
+	pool := &sync.Pool{
+		New: func() interface{} {
+			fmt.Println("Create a new object.")
+			return 100
+		},
+	}
+	v := pool.Get().(int)
+	fmt.Println(v)
+	pool.Put(3)
+	v1, _ := pool.Get().(int)
+	fmt.Println(v1)
+	runtime.GC()
+	v2, _ := pool.Get().(int)
+	fmt.Println(v2)
+}
+
+func TestSyncPoolInMultiGroutine(t *testing.T) {
+	pool := &sync.Pool{
+		New: func() interface{} {
+			fmt.Println("Create a new object.")
+			return 10
+		},
+	}
+
+	pool.Put(100)
+	pool.Put(100)
+	pool.Put(100)
+
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(id int) {
+			fmt.Println(pool.Get())
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}
