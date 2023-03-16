628b74fa3a7ec6e310b1d73ff6fe2792c286c30e
zzuse <zzuse@outlook.com>
2023-03-15 22:40:10 -0500

    reflect unsafe

---
+++ Golang/src/ch29/reflect/reflect_test.go
@@ -0,0 +1,120 @@
+package reflect
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+type Employee struct {
+	EmployeeID string
+	Name       string `format:"normal"`
+	Age        int
+}
+
+func (e *Employee) UpdateAge(newVal int) {
+	e.Age = newVal
+}
+
+type Customer struct {
+	CookieID string
+	Name     string
+	Age      int
+}
+
+func TestInvokeByName(t *testing.T) {
+	e := &Employee{"1", "Mike", 30}
+	t.Logf("Name: value(%[1]v), Type(%[1]T) ", reflect.ValueOf(*e).FieldByName("Name"))
+	if nameField, ok := reflect.TypeOf(*e).FieldByName("Name"); !ok {
+		t.Error("Failed to get 'Name' field.")
+	} else {
+		t.Log("Tag:format", nameField.Tag.Get("format"))
+	}
+	reflect.ValueOf(e).MethodByName("UpdateAge").Call([]reflect.Value{reflect.ValueOf(1)})
+	t.Log("Updated Age:", e)
+}
+
+func TestTypeAndValue(t *testing.T) {
+	var f int64 = 10
+	t.Log(reflect.TypeOf(f), reflect.ValueOf(f))
+	t.Log(reflect.ValueOf(f).Type())
+}
+
+func CheckType(v interface{}) {
+	t := reflect.TypeOf(v)
+	switch t.Kind() {
+	case reflect.Float32, reflect.Float64:
+		fmt.Println("Float")
+	case reflect.Int, reflect.Int32, reflect.Int64:
+		fmt.Println("Integer")
+	default:
+		fmt.Println("Unknown", t)
+	}
+}
+
+func TestBasicType(t *testing.T) {
+	var f float64 = 12
+	CheckType(f)
+	CheckType(&f)
+}
+
+func TestDeepEqual(t *testing.T) {
+	a := map[int]string{1: "out", 2: "two", 3: "three"}
+	b := map[int]string{1: "out", 2: "two", 3: "three"}
+	fmt.Println(reflect.DeepEqual(a, b))
+
+	s1 := []int{1, 2, 3}
+	s2 := []int{1, 2, 3}
+	s3 := []int{2, 3, 1}
+	t.Log("s1 == s2?", reflect.DeepEqual(s1, s2))
+	t.Log("s1 == s3?", reflect.DeepEqual(s1, s3))
+
+	c1 := Customer{"1", "Mike", 40}
+	c2 := Customer{"1", "Mike", 40}
+	fmt.Println(c1 == c2)
+	fmt.Println(reflect.DeepEqual(c1, c2))
+}
+
+func fillBySettings(st interface{}, settings map[string]interface{}) error {
+	if reflect.TypeOf(st).Kind() != reflect.Ptr {
+		if reflect.TypeOf(st).Elem().Kind() != reflect.Struct {
+			return errors.New("the first param should e a pointer to the struct type")
+		}
+	}
+
+	if settings == nil {
+		return errors.New("settings is nil.")
+	}
+
+	var (
+		field reflect.StructField
+		ok    bool
+	)
+
+	for k, v := range settings {
+		if field, ok = (reflect.ValueOf(st)).Elem().Type().FieldByName(k); !ok {
+			continue
+		}
+		if field.Type == reflect.TypeOf(v) {
+			vstr := reflect.ValueOf(st)
+			vstr = vstr.Elem()
+			vstr.FieldByName(k).Set(reflect.ValueOf(v))
+		}
+	}
+	return nil
+}
+
+func TestFillNameAndAge(t *testing.T) {
+	settings := map[string]interface{}{"Name": "Mike", "Age": 40}
+	e := Employee{}
+	if err := fillBySettings(&e, settings); err != nil {
+		t.Fatal(err)
+	}
+	t.Log(e)
+	c := new(Customer)
+	if err := fillBySettings(c, settings); err != nil {
+		t.Fatal(err)
+	}
+	t.Log(*c)
+}
