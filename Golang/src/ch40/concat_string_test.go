584dbe48c2ae1d8ea1996fbe9d44570f53e99361
zzuse <zzuse@outlook.com>
2023-03-20 01:20:55 -0500

    slice string

---
+++ Golang/src/ch40/concat_string_test.go
@@ -0,0 +1,59 @@
+package concat_string
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+const numbers = 100
+
+func BenchmarkSprintf(b *testing.B) {
+	b.ResetTimer()
+	for idx := 0; idx < b.N; idx++ {
+		var s string
+		for i := 0; i < numbers; i++ {
+			s = fmt.Sprintf("%v%v", s, i)
+		}
+	}
+	b.StopTimer()
+}
+
+// before go 1.10
+func BenchmarkBytesBuf(b *testing.B) {
+	b.ResetTimer()
+	for idx := 0; idx < b.N; idx++ {
+		var buf bytes.Buffer
+		for i := 0; i < numbers; i++ {
+			buf.WriteString(strconv.Itoa(i))
+		}
+		_ = buf.String()
+	}
+	b.StopTimer()
+}
+
+func BenchmarkStringAdd(b *testing.B) {
+	b.ResetTimer()
+	for idx := 0; idx < b.N; idx++ {
+		var s string
+		for i := 0; i < numbers; i++ {
+			s += strconv.Itoa(i)
+		}
+	}
+	b.StopTimer()
+}
+
+// recently
+func BenchmarkStringBuilder(b *testing.B) {
+	b.ResetTimer()
+	for idx := 0; idx < b.N; idx++ {
+		var builder strings.Builder
+		for i := 0; i < numbers; i++ {
+			builder.WriteString(strconv.Itoa(i))
+		}
+		_ = builder.String()
+	}
+	b.StopTimer()
+}
