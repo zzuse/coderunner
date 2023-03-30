bf47881bf2df8718d9ec9f21c294e18f5c3dd209
zzuse <zzuse@outlook.com>
2023-03-30 00:18:50 -0500

    slice cap

--- Golang/src/ch41/var_test/var_test.go
+++ Golang/src/ch41/var_test/var_test.go
@@ -52,7 +52,20 @@ func TestIntConv(t *testing.T) {
 	fmt.Print(srcInt, disInt)
 }
 
-func TestSlice(t *testing.T) {
+var container = []string{"zero", "one", "two"}
+
+func TestContainerType(t *testing.T) {
+	container := map[int]string{0: "zero", 1: "one", 2: "two"}
+	fmt.Printf("The element is %q.\n", container[1])
+	value, ok := interface{}(container).([]string)
+	if ok == true {
+		print(value)
+	} else if value == nil {
+		print("not string slice")
+	}
+}
+
+func TestSliceCapacity(t *testing.T) {
 	s3 := []int{1, 2, 3, 4, 5, 6, 7, 8}
 	s4 := s3[3:6]
 	fmt.Printf("The length of s4: %d\n", len(s4))
