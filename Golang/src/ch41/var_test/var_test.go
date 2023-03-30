a4aa605905bed97b62ff263aa4b85266289b2a51
zzuse <zzuse@outlook.com>
2023-03-29 23:58:53 -0500

    slice capcity

--- Golang/src/ch41/var_test/var_test.go
+++ Golang/src/ch41/var_test/var_test.go
@@ -51,3 +51,11 @@ func TestIntConv(t *testing.T) {
 	disInt := int8(srcInt)
 	fmt.Print(srcInt, disInt)
 }
+
+func TestSlice(t *testing.T) {
+	s3 := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	s4 := s3[3:6]
+	fmt.Printf("The length of s4: %d\n", len(s4))
+	fmt.Printf("The capacity of s4: %d\n", cap(s4))
+	fmt.Printf("The value of s4: %d\n", s4)
+}
