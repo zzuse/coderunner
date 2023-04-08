9b450a757dc39aa41a2e0a04aeb0e3816c614b79
zzuse <zzuse@outlook.com>
2023-04-07 23:58:40 -0500

    modify test

--- Golang/src/ch43/func_test/func_test.go
+++ Golang/src/ch43/func_test/func_test.go
@@ -48,3 +48,47 @@ func TestAdd(t *testing.T) {
 	result, err = add(x, y)
 	fmt.Printf("The result: %d (error: %v\n", result, err)
 }
+
+func modifyArray(a [3]string) [3]string {
+	a[1] = "x"
+	return a
+}
+
+func TestArray(t *testing.T) {
+	array1 := [3]string{"a", "b", "c"}
+	fmt.Printf("The array: %v\n", array1)
+	array2 := modifyArray(array1)
+	fmt.Printf("The modified array: %v\n", array2)
+	fmt.Printf("The original array: %v\n", array1)
+}
+
+func modifySlice(a []string) []string {
+	a[1] = "1"
+	return a
+}
+
+func TestSlice(t *testing.T) {
+	slice1 := []string{"x", "y", "z"}
+	fmt.Printf("The slice: %v\n", slice1)
+	slice2 := modifySlice(slice1)
+	fmt.Printf("The modified slice: %v\n", slice2)
+	fmt.Printf("The original slice: %v\n", slice1)
+}
+
+func modifyComplexArray(a [3][]string) [3][]string {
+	a[1][1] = "s"
+	a[2] = []string{"o", "p", "q"}
+	return a
+}
+
+func TestComplexArray(t *testing.T) {
+	complexArray1 := [3][]string{
+		[]string{"d", "e", "f"},
+		[]string{"g", "h", "i"},
+		[]string{"j", "k", "l"},
+	}
+	fmt.Printf("The com array %v\n", complexArray1)
+	complexArray2 := modifyComplexArray(complexArray1)
+	fmt.Printf("The modified: %v\n", complexArray2)
+	fmt.Printf("The original: %v\n", complexArray1)
+}
