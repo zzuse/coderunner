515665f567ad6785ffdf835722256334109b9cdf
zzuse <zzuse@outlook.com>
2023-04-09 23:59:25 -0500

    animal struct

--- Golang/src/ch44/struct_test/struct_test.go
+++ Golang/src/ch44/struct_test/struct_test.go
@@ -15,11 +15,42 @@ type AnimalCategory struct {
 	species string
 }
 
+type Animal struct {
+	scientificName string
+	AnimalCategory
+}
+
+type Cat struct {
+	name string
+	Animal
+}
+
 func (ac AnimalCategory) String() string {
 	return fmt.Sprintf("%s%s%s%s%s%s%s", ac.kingdom, ac.phylum, ac.class, ac.order, ac.family, ac.genus, ac.species)
 }
 
-func TestAnimal(t *testing.T) {
+func (a Animal) Category() string {
+	return a.AnimalCategory.String()
+}
+
+func (a Animal) String() string {
+	return fmt.Sprintf("%s (category:%s)", a.scientificName, a.AnimalCategory)
+}
+
+func (cat Cat) String() string {
+	return fmt.Sprintf("%s (category: %s, name: %q)", cat.scientificName, cat.Animal.AnimalCategory, cat.name)
+}
+
+func TestAnimalCategory(t *testing.T) {
 	category := AnimalCategory{species: "cat"}
 	fmt.Printf("The animal category: %s\n", category)
+	animal := Animal{
+		scientificName: "American Shorthair",
+		AnimalCategory: category,
+	}
+	cat := Cat{
+		name:   "cat",
+		Animal: animal,
+	}
+	fmt.Printf("The animal: %s\n", cat)
 }
