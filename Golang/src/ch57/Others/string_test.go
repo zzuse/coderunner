71bd4b1ddd5f22ba9dbafac7052ebeb0e8711e4d
zzuse <zzuse@outlook.com>
2023-05-11 23:36:20 -0500

    reader

--- Golang/src/ch57/Others/string_test.go
+++ Golang/src/ch57/Others/string_test.go
@@ -2,6 +2,7 @@ package Others
 
 import (
 	"fmt"
+	"io"
 	"strings"
 	"testing"
 )
@@ -52,4 +53,36 @@ func TestString(t *testing.T) {
 	builder1.Reset()
 	builder5 := builder1
 	builder5.Grow(1)
+
+	reader1 := strings.NewReader("NewReader returns a new Reader reading from s. " +
+		"It is similar to bytes.NewBufferString but more efficient and read-only.")
+	fmt.Printf("The size of reader: %d\n", reader1.Size())
+	fmt.Printf("The reading index in reader: %d\n",
+		reader1.Size()-int64(reader1.Len()))
+	buf1 := make([]byte, 47)
+	n, _ := reader1.Read(buf1)
+	fmt.Printf("%d bytes were read. (call Read)\n", n)
+	fmt.Printf("The reading index in reader: %d\n",
+		reader1.Size()-int64(reader1.Len()))
+	fmt.Println()
+
+	buf2 := make([]byte, 21)
+	offset1 := int64(64)
+	n, _ = reader1.ReadAt(buf2, offset1)
+	fmt.Printf("%d bytes were read. (call ReadAt, offset: %d)\n", n, offset1)
+	fmt.Printf("The reading index in reader: %d\n",
+		reader1.Size()-int64(reader1.Len()))
+	fmt.Println()
+
+	offset2 := int64(17)
+	expectIndex := reader1.Size() - int64(reader1.Len()) + offset2
+	fmt.Printf("Seek with offset %d and whence %d ...\n", offset2, io.SeekCurrent)
+	readingIndex, _ := reader1.Seek(offset2, io.SeekCurrent)
+	fmt.Printf("The reading index in reader: %d (returned by Seek)\n", readingIndex)
+	fmt.Printf("The reading index in reader: %d (compute by me)\n", expectIndex)
+
+	n, _ = reader1.Read(buf2)
+	fmt.Printf("%d bytes were read. (call Read)\n", n)
+	fmt.Printf("The reading index in reader: %d\n",
+		reader1.Size()-int64(reader1.Len()))
 }
