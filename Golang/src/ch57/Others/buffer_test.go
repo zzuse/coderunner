f986e9ecad492cdd5930c4fc3c213360579f1bf9
zzuse <zzuse@outlook.com>
2023-05-16 23:58:04 -0500

    buffer

--- Golang/src/ch57/Others/buffer_test.go
+++ Golang/src/ch57/Others/buffer_test.go
@@ -117,4 +117,40 @@ func TestBuffer2(t *testing.T) {
 	unreadBytes = unreadBytes[:cap(unreadBytes)]
 	fmt.Printf("The unread bytes of the buffer: %v\n", unreadBytes)
 	fmt.Printf("\n\n")
+
+	contents = "12"
+	buffer2 := bytes.NewBufferString(contents)
+	fmt.Printf("The length of new buffer with contents %q: %d\n", contents, buffer2.Len())
+	fmt.Printf("The capacity of new buffer with contents %q: %d\n", contents, buffer2.Cap())
+	fmt.Println()
+
+	nextBytes := buffer2.Next(2)
+	fmt.Printf("The next bytes of the buffer: %v\n", nextBytes)
+	fmt.Println()
+
+	contents = "34567"
+	fmt.Printf("Write contents %q ...\n", contents)
+	buffer2.WriteString(contents)
+	fmt.Printf("The length of new buffer with contents %q: %d\n", contents, buffer2.Len())
+	fmt.Printf("The capacity of new buffer with contents %q: %d\n", contents, buffer2.Cap())
+	fmt.Println()
+
+	nextBytes = nextBytes[:cap(nextBytes)]
+	fmt.Printf("Set a byte of the buffer: %v\n", nextBytes)
+	fmt.Println()
+
+	value = byte('X')
+	fmt.Printf("Set a byte in the next bytesbto %v\n", value)
+	nextBytes[len(nextBytes)-2] = value
+	fmt.Printf("The unread bytes of the buffer: %v\n", buffer2.Bytes())
+	fmt.Println()
+
+	contents = "89101112"
+	fmt.Printf("Write contents %q ...\n", contents)
+	buffer2.WriteString(contents)
+	fmt.Printf("The capacity of buffer: %d\n", buffer2.Cap())
+	fmt.Println()
+
+	nextBytes = nextBytes[:cap(nextBytes)]
+	fmt.Printf("The next bytes of the buffer: %v\n", nextBytes)
 }
