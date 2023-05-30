b63bf22bca680bfe0a194b2c581caac7f47233bb
zzuse <zzuse@outlook.com>
2023-05-30 00:57:38 -0500

    socket

--- Golang/src/ch57/Others/socket_test.go
+++ Golang/src/ch57/Others/socket_test.go
@@ -9,6 +9,7 @@ import (
 	"runtime"
 	"syscall"
 	"testing"
+	"time"
 )
 
 func TestSocket(t *testing.T) {
@@ -91,5 +92,42 @@ User-Agent: Dialer/%s
 	}
 	fmt.Printf("The first line of response:\n%s\n", line2)
 	fmt.Println()
+}
 
+type dialArgs struct {
+	network string
+	address string
+	timeout time.Duration
+}
+
+func TestSocket3(t *testing.T) {
+	dialArgsList := []dialArgs{
+		{"tcp",
+			"google.ca:80",
+			time.Millisecond * 500},
+		{"tcp",
+			"google.ca:80",
+			time.Second * 2},
+		{
+			"tcp",
+			"google.ca:80",
+			time.Minute * 4,
+		},
+	}
+	for _, args := range dialArgsList {
+		fmt.Printf("Dial %q with network %q and timeout %s...\n", args.address, args.network, args.timeout)
+		ts1 := time.Now()
+		conn, err := net.DialTimeout(args.network, args.address, args.timeout)
+		ts2 := time.Now()
+		fmt.Printf("Elapsed time: %s\n", time.Duration(ts2.Sub(ts1)))
+		if err != nil {
+			fmt.Printf("dial error: %v\n", err)
+			fmt.Println()
+			continue
+		}
+		defer conn.Close()
+		fmt.Printf("The local address: %s\n", conn.LocalAddr())
+		fmt.Printf("The remote address: %s\n", conn.RemoteAddr())
+		fmt.Println()
+	}
 }
