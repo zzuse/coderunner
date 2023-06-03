b4f10ae928c0609b41c065eccad21ae2cfe8a6ff
zzuse <zzuse@outlook.com>
2023-06-03 01:49:23 -0500

    http

--- Golang/src/ch57/Others/http_test.go
+++ Golang/src/ch57/Others/http_test.go
@@ -3,7 +3,10 @@ package Others
 import (
 	"fmt"
 	"net/http"
+	"strings"
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestHttp(t *testing.T) {
@@ -33,4 +36,57 @@ func TestHttp(t *testing.T) {
 	defer resp2.Body.Close()
 	line2 := resp2.Proto + " " + resp2.Status
 	fmt.Printf("Response: %s\n", line2)
+}
+
+var domains = []string{
+	"google.com",
+	"google.com.hk",
+	"google.cn",
+	"google.ca",
+	"go.dev",
+}
+
+func TestHttp2(t *testing.T) {
+	myTransport := &http.Transport{
+		Proxy:                 http.ProxyFromEnvironment,
+		MaxConnsPerHost:       2,
+		MaxIdleConns:          10,
+		MaxIdleConnsPerHost:   2,
+		IdleConnTimeout:       30 * time.Second,
+		ResponseHeaderTimeout: 0,
+		ExpectContinueTimeout: 1 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+	}
+	myClient := http.Client{
+		Transport: myTransport,
+		Timeout:   20 * time.Second,
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(domains))
+	for _, domain := range domains {
+		go func(domain string) {
+			var logBuf strings.Builder
+			var diff time.Duration
+			defer func() {
+				logBuf.WriteString(
+					fmt.Sprintf("(elapsed time: %s)\n", diff))
+				fmt.Println(logBuf.String())
+				wg.Done()
+			}()
+			url := "https://" + domain
+			logBuf.WriteString(fmt.Sprintf("Send request to %q\n", url))
+			t1 := time.Now()
+			resp, err := myClient.Get(url)
+			diff = time.Now().Sub(t1)
+			if err != nil {
+				logBuf.WriteString(fmt.Sprintf("Error: %v\n", err))
+				return
+			}
+			defer resp.Body.Close()
+			line2 := resp.Proto + " " + resp.Status
+			logBuf.WriteString(fmt.Sprintf("Response: %s\n", line2))
+		}(domain)
+	}
+	wg.Wait()
 }
