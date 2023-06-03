8fc5d5062a7a3278bcf1dd411ea3d25eac06711f
zzuse <zzuse@outlook.com>
2023-06-02 23:55:02 -0500

    http

--- Golang/src/ch57/Others/http_test.go
+++ Golang/src/ch57/Others/http_test.go
@@ -17,4 +17,20 @@ func TestHttp(t *testing.T) {
 		return
 	}
 	defer resp1.Body.Close()
+	line1 := resp1.Proto + " " + resp1.Status
+	fmt.Printf("Response: %s\n", line1)
+	fmt.Printf("Response: %q\n", line1)
+	fmt.Println()
+
+	url2 := "https://go.dev"
+	fmt.Printf("Send request to %q\n", url2)
+	var httpClient1 http.Client
+	resp2, err := httpClient1.Get(url2)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+	defer resp2.Body.Close()
+	line2 := resp2.Proto + " " + resp2.Status
+	fmt.Printf("Response: %s\n", line2)
 }
