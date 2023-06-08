f403f4ad1d56bf306cec4bbfcd17e5c5e7e8708b
zzuse <zzuse@outlook.com>
2023-06-08 00:16:45 -0500

    perf

--- Golang/src/ch58/perf/profile_test.go
+++ Golang/src/ch58/perf/profile_test.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	"go_learning/src/ch58/perf/common"
 	"os"
+	"runtime"
 	"runtime/pprof"
 	"testing"
 )
 
 var (
-	profileName = "cpu_profile.out"
+	cpuProfileName = "cpu_profile.out"
 )
 
 func startCPUProfile(f *os.File) error {
@@ -25,7 +26,7 @@ func stopCPUProfile() {
 }
 
 func TestCpuProfile(t *testing.T) {
-	f, err := common.CreateFile("", profileName)
+	f, err := common.CreateFile("", cpuProfileName)
 	if err != nil {
 		fmt.Printf("CPU profile creation error: %v\n", err)
 		return
@@ -42,6 +43,36 @@ func TestCpuProfile(t *testing.T) {
 	stopCPUProfile()
 }
 
+var (
+	memProfileName = "mem_profile.out"
+	memProfileRate = 8
+)
+
+func startMemProfile() {
+	runtime.MemProfileRate = memProfileRate
+}
+
+func stopMemProfile(f *os.File) error {
+	if f == nil {
+		return errors.New("nil file")
+	}
+	return pprof.WriteHeapProfile(f)
+}
+
 func TestMemProfile(t *testing.T) {
-	f, err := common.CreateFile("", profileName)
+	f, err := common.CreateFile("", memProfileName)
+	if err != nil {
+		fmt.Printf("memory profile creation error: %v\n", err)
+		return
+	}
+	defer f.Close()
+	startMemProfile()
+	if err = common.Execute(common.MemProfile, 10); err != nil {
+		fmt.Printf("execute error: %v\n", err)
+		return
+	}
+	if err := stopMemProfile(f); err != nil {
+		fmt.Printf("memory profile stop error: %v\n", err)
+		return
+	}
 }
