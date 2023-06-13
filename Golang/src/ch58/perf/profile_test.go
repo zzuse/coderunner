4012908a7dd74c09169488eea627107a91520f25
zzuse <zzuse@outlook.com>
2023-06-12 23:55:52 -0500

    perf

--- Golang/src/ch58/perf/profile_test.go
+++ Golang/src/ch58/perf/profile_test.go
@@ -4,10 +4,14 @@ import (
 	"errors"
 	"fmt"
 	"go_learning/src/ch58/perf/common"
+	"log"
+	"net/http"
 	"os"
+	"path/filepath"
 	"runtime"
 	"runtime/pprof"
 	"testing"
+	"time"
 )
 
 var (
@@ -95,5 +99,105 @@ func stopBlockProfile(f *os.File) error {
 }
 
 func TestBlockProfile(t *testing.T) {
+	f, err := common.CreateFile("", blockProfileName)
+	if err != nil {
+		fmt.Printf("block profile creation error: %v\n", err)
+		return
+	}
+	defer f.Close()
+	startBlockProfile()
+	if err = common.Execute(common.BlockProfile, 10); err != nil {
+		fmt.Printf("execute error: %v\n", err)
+		return
+	}
+	if err := stopBlockProfile(f); err != nil {
+		fmt.Printf("block profile stop error: %v\n", err)
+		return
+	}
+}
 
+var profileNames = []string{
+	"goroutine",
+	"heap",
+	"allocs",
+	"threadcreate",
+	"block",
+	"mutex",
+}
+
+var profileOps = map[string]common.OpFunc{
+	"goroutine":    common.BlockProfile,
+	"heap":         common.MemProfile,
+	"allocs":       common.MemProfile,
+	"threadcreate": common.BlockProfile,
+	"block":        common.BlockProfile,
+	"mutex":        common.BlockProfile,
+}
+
+var debugOpts = []int{
+	0,
+	1,
+	2,
+}
+
+func genProfile(dir string, name string, debug int) error {
+	fmt.Printf("Generating %s profile (debug:%d)\n", name, debug)
+	fileName := fmt.Sprintf("%s_%d.out", name, debug)
+	f, err := common.CreateFile(dir, fileName)
+	if err != nil {
+		fmt.Printf("create error: %v (%s)\n", err, fileName)
+		return err
+	}
+	defer f.Close()
+	if err = common.Execute(profileOps[name], 10); err != nil {
+		fmt.Printf("execute error: %v (%s)\n", err, fileName)
+		return err
+	}
+	profile := pprof.Lookup(name)
+	err = profile.WriteTo(f, debug)
+	if err != nil {
+		fmt.Printf("write error: %v (%s)\n", err, fileName)
+		return err
+	}
+	return nil
+}
+
+func createDir() (string, error) {
+	currDir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	path := filepath.Join(currDir, "profiles")
+	err = os.Mkdir(path, 0766)
+	if err != nil && !os.IsExist(err) {
+		return "", err
+	}
+	return path, nil
+}
+
+func prepare() {
+	runtime.MemProfileRate = 8
+	runtime.SetBlockProfileRate(2)
+}
+
+func TestProfile(t *testing.T) {
+	prepare()
+	dir, err := createDir()
+	if err != nil {
+		fmt.Printf("dir creation error: %v\n", err)
+		return
+	}
+	for _, name := range profileNames {
+		for _, debug := range debugOpts {
+			err = genProfile(dir, name, debug)
+			if err != nil {
+				return
+			}
+			time.Sleep(time.Millisecond)
+		}
+	}
+}
+
+func TestListen(t *testing.T) {
+	log.Println(http.ListenAndServe("localhost:8082", nil))
 }
