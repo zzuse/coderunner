56e455ebd8b842863517ada1ccc019465b6ee038
zzuse <zzuse@outlook.com>
2023-11-01 23:57:10 -0500

    Merge Sorted Array

--- LeetCode/problems/array101.h
+++ LeetCode/problems/array101.h
@@ -55,6 +55,29 @@ public:
             }
         }
     }
+
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
+    {
+        vector<int> temp;
+        for (int i = 0; i < m; i++) temp.push_back(nums1[i]);
+        int i = 0, j = 0, k = 0;
+        while (i < m && temp[i]) {
+            if (temp[i] <= nums2[j]) {
+                nums1[k] = temp[i];
+                k++;
+                i++;
+            } else {
+                nums1[k] = nums2[j];
+                k++;
+                j++;
+            }
+        }
+        while (j < n) {
+            nums1[k] = nums2[j];
+            k++;
+            j++;
+        }
+    }
 };
 
 class SolutionBetter {
