b4236c6ec367a514ef942e73c4f3d802e42914eb
zzuse <zzuse@outlook.com>
2023-11-11 23:57:21 -0600

    Remove Element

--- LeetCode/cases/array_test.cpp
+++ LeetCode/cases/array_test.cpp
@@ -72,17 +72,17 @@ TEST_CASE("Remove Element", "[array]")
 {
     SECTION("Remove from start")
     {
-        // int[] nums = [...]; // Input array
-        // int val = ...; // Value to remove
-        // int[] expectedNums = [...]; // The expected answer with correct length.
-        //                             // It is sorted with no values equaling val.
+        int[] nums = [ 3, 2, 2, 3 ];      // Input array
+        int val = 3;                      // Value to remove
+        int[] expectedNums = [ 2, 2 ];    // The expected answer with correct length.
+                                          // It is sorted with no values equaling val.
 
-        // int k = removeElement(nums, val); // Calls your implementation
+        int k = removeElement(nums, val); // Calls your implementation
 
-        // assert k == expectedNums.length;
-        // sort(nums, 0, k); // Sort the first k elements of nums
-        // for (int i = 0; i < actualLength; i++) {
-        //     assert nums[i] == expectedNums[i];
-        // }
+        assert k == expectedNums.length;
+        sort(nums, 0, k); // Sort the first k elements of nums
+        for (int i = 0; i < 2; i++) {
+            assert nums[i] == expectedNums[i];
+        }
     }
 }
