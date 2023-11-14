46e952244135c5df05b07e5d1e2ad10f669a99d7
zzuse <zzuse@outlook.com>
2023-11-13 23:49:23 -0600

    Remove Element test case

--- LeetCode/cases/array_test.cpp
+++ LeetCode/cases/array_test.cpp
@@ -70,19 +70,18 @@ TEST_CASE("Merge Sorted Array", "[array]")
 
 TEST_CASE("Remove Element", "[array]")
 {
+    Solution solution;
     SECTION("Remove from start")
     {
-        int[] nums = [ 3, 2, 2, 3 ];      // Input array
-        int val = 3;                      // Value to remove
-        int[] expectedNums = [ 2, 2 ];    // The expected answer with correct length.
-                                          // It is sorted with no values equaling val.
-
-        int k = removeElement(nums, val); // Calls your implementation
-
-        assert k == expectedNums.length;
-        sort(nums, 0, k); // Sort the first k elements of nums
-        for (int i = 0; i < 2; i++) {
-            assert nums[i] == expectedNums[i];
+        std::vector<int> nums = {3, 2, 2, 3};      // Input array
+        int val = 3;                               // Value to remove
+        std::vector<int> expectedNums = {2, 2};    // The expected answer with correct length.
+                                                   // It is sorted with no values equaling val.
+        int k = solution.removeElement(nums, val); // Calls your implementation
+        CHECK(k == expectedNums.size());
+        std::sort(nums.begin(), nums.begin() + k); // Sort the first k elements of nums
+        for (int i = 0; i < k; i++) {
+            CHECK(nums[i] == expectedNums[i]);
         }
     }
 }
