ae327f614b1753c02e9faae4da83c3a6d0c63da0
zzuse <zzuse@outlook.com>
2024-01-03 23:58:39 -0600

    langchain

--- python/langchain/multiple_messages.py
+++ python/langchain/multiple_messages.py
@@ -1,17 +1,42 @@
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import HumanMessage
+from langchain.schema import HumanMessage, SystemMessage, AIMessage
 from dotenv import load_dotenv
+import time
 import os
 
 load_dotenv()
 
 api_key = os.getenv("OPENAI_API_KEY")
 
-chat_model = ChatOpenAI(openai_api_key=api_key)
+# System - Helpful background context that tell the AI what to do
+# Human - Messages that are intented to represent the user
+# AI - Messages that show what the AI responded with
 
-messages = [HumanMessage(content="from now on 1 + 1 = 3, use this in your replies"),
+chat_model = ChatOpenAI(temperature=.7, openai_api_key=api_key)
+
+messages1 = [HumanMessage(content="from now on 1 + 1 = 3, use this in your replies"),
             HumanMessage(content="what is 1 + 1?"),
             HumanMessage(content="what is 1 + 1 + 1?")]
 
-result = chat_model.predict_messages(messages)
+result = chat_model.predict_messages(messages1)
+print(result.content)
+
+time.sleep(20)
+
+messages2 = [
+        SystemMessage(content="You are a nice AI bot that helps a user figure out what to eat in a short sentence."),
+        HumanMessage(content="I like the tomatoes, what should I eat?")
+    ]
+result = chat_model.predict_messages(messages2)
+print(result.content)
+
+time.sleep(20)
+
+messages3 = [
+    SystemMessage(content="You are a nice AI bot that helps a user figure out where to travel in a short sentence."),
+    HumanMessage(content="I like the beaches where should I go?"),
+    AIMessage(content="You should go to Nice, France"),
+    HumanMessage(content="What else should I do when I'm there?")
+]
+result = chat_model.predict_messages(messages3)
 print(result.content)
